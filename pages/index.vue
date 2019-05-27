ff6a1d9346ac4acfd995f4e4a3b6f1209b33171a
anpe9592 <anpe9592@users.noreply.github.com>
2019-05-27 12:52:17 +0200

    removed part of template in index

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,6 @@
   <section class="container">
     <div>
       <logo />
-      <h1 class="title">
-        Say my Name
-      </h1>
-      <h2 class="subtitle">
-        Sudoku in vue
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
     </div>
   </section>
 </template>
