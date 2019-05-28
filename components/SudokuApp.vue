0f223975d5b1f6241607d26faf86dcd850db5677
anpe9592 <anpe9592@users.noreply.github.com>
2019-05-28 16:16:21 +0200

    added an array filled with a sudoku

--- components/SudokuApp.vue
+++ components/SudokuApp.vue
@@ -1,15 +1,59 @@
 <template>
   <div class="Sudoku">
-    text
+    <div v-for="(number, index) in num" :key="index">{{ number }}</div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: [
+        [8, 0, 0],
+        [0, 0, 0],
+        [0, 1, 0],
+
+        [4, 0, 6],
+        [0, 0, 0],
+        [0, 0, 0],
+
+        [0, 0, 7],
+        [4, 0, 0],
+        [6, 5, 0],
+
+        [5, 0, 9],
+        [0, 0, 0],
+        [0, 4, 8],
+
+        [0, 3, 0],
+        [0, 7, 0],
+        [0, 2, 0],
+
+        [7, 8, 0],
+        [0, 0, 0],
+        [1, 0, 3],
+
+        [0, 5, 2],
+        [0, 0, 1],
+        [3, 0, 0],
+
+        [0, 0, 0],
+        [0, 0, 0],
+        [9, 0, 2],
+
+        [0, 9, 0],
+        [0, 0, 0],
+        [0, 0, 5]
+      ]
+    }
+  }
+}
+</script>
 
 <style>
 .Sudoku {
   display: inline-block;
   position: relative;
   overflow: hidden;
-  height: 180px;
-  width: 245px;
 }
 </style>
