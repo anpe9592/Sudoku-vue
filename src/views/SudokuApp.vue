cfa36f57fa1594c5b3798ec6435d66aca571117f
anpe9592 <anpe9592@users.noreply.github.com>
2019-05-31 18:37:45 +0200

    change HomeWorld.vue to Sudoku.vue

--- src/views/SudokuApp.vue
+++ src/views/SudokuApp.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="sudokuapp">
-    <HelloWorld />
+    <Sudoku />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Sudoku from '@/components/Sudoku.vue'
 
 export default {
   name: 'sudokuapp',
   components: {
-    HelloWorld
+    Sudoku
   }
 }
 </script>
