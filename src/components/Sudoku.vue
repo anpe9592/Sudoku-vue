2c7ba0884cc2a10e95190aeae54b1012c6349b1e
anpe9592 <anpe9592@users.noreply.github.com>
2019-06-18 23:43:05 +0200

    created two separate methods for creating board and shadow board

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -29,6 +29,11 @@ export default {
     },
 
     setupBoard () {
+      this.createBoard()
+      this.createShadowBoard()
+    },
+
+    createBoard () {
       this.items = [
         8, 0, 0, 4, 0, 6, 0, 0, 7,
         0, 0, 0, 0, 0, 0, 4, 0, 0,
@@ -42,6 +47,10 @@ export default {
         0, 0, 1, 0, 0, 0, 0, 0, 0,
         3, 0, 0, 9, 0, 2, 0, 0, 5
       ]
+    },
+
+    createShadowBoard () {
+      this.shadowItems = this.items
     }
   }
 }
