09675ab1f1dc0b4aa61124cca19c0761942d48a1
anpe9592 <anpe9592@users.noreply.github.com>
2019-06-20 00:08:32 +0200

    copy items to shadowItems correctly now

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -33,11 +33,11 @@ export default {
     },
 
     setupBoard () {
-      this.createShadowBoard()
       this.createBoard()
+      this.createShadowBoard()
     },
 
-    createShadowBoard () {
+    createBoard () {
       this.items = [
         8, 0, 0, 4, 0, 6, 0, 0, 7,
         0, 0, 0, 0, 0, 0, 4, 0, 0,
@@ -53,20 +53,8 @@ export default {
       ]
     },
 
-    createBoard () {
-      this.shadowItems = [
-        8, 0, 0, 4, 0, 6, 0, 0, 7,
-        0, 0, 0, 0, 0, 0, 4, 0, 0,
-        0, 1, 0, 0, 0, 0, 6, 5, 0,
-
-        5, 0, 9, 0, 3, 0, 7, 8, 0,
-        0, 0, 0, 0, 7, 0, 0, 0, 0,
-        0, 4, 8, 0, 2, 0, 1, 0, 3,
-
-        0, 5, 2, 0, 0, 0, 0, 9, 0,
-        0, 0, 1, 0, 0, 0, 0, 0, 0,
-        3, 0, 0, 9, 0, 2, 0, 0, 5
-      ]
+    createShadowBoard () {
+      this.shadowItems = [...this.items]
     }
   }
 }
