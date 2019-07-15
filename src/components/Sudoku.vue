e800662fa322326b3c2c3a5be62b96bca9426534
anpe9592 <anpe9592@users.noreply.github.com>
2019-07-15 21:07:43 +0200

    remove log in sudoku.vue
    remove log in sudoku.vue

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -42,13 +42,11 @@ export default {
     },
 
     removeZeros () {
-      let count = 0
       for (let index = 0; index < this.items.length; index++) {
         if (this.items[index] < 1) {
           this.items[index] = ''
         }
       }
-      console.log(count)
     },
 
     createBoard () {
